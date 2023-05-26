05b326e45577df9ae8a91a594b1e26b875c51f98
sam <s.mcdonald@outlook.com.au>
2023-05-26 20:39:43 +1000

    DEV - Added JsonAsserter

---
+++ src/Assert/JsonAsserter.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SamMcDonald\Jason\Assert;
+
+use SamMcDonald\Jason\Exceptions\InvalidArgumentException;
+
+class JsonAsserter
+{
+    private function __construct()
+    {
+    }
+
+    public static function assertStringIsValidJson(string $json, string $message = ''): void
+    {
+        if (!static::validateJsonString($json)) {
+            static::throwInvalidArgument(
+                $message ?: 'Expected valid Json. Received Invalid Json structure'
+            );
+        }
+    }
+
+    public static function throwInvalidArgument(string $message): void
+    {
+        throw new InvalidArgumentException($message);
+    }
+
+    protected static function validateJsonString(string $json): bool
+    {
+        if (!empty($json)) {
+            return false;
+        }
+
+        \is_array(\json_decode($json, true));
+
+        return json_last_error() === JSON_ERROR_NONE;
+    }
+}
