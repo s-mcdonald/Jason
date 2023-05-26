5f047eed23a4518fad750f2c310b442803a4d567
sam <s.mcdonald@outlook.com.au>
2023-05-27 00:23:40 +1000

    DEV - Add decodeValidationError on JsonValidator

--- src/Validator/JsonValidator.php
+++ src/Validator/JsonValidator.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace SamMcDonald\Jason\Validator;
 
+use SamMcDonald\Jason\Exceptions\JsonDecodeException;
+
 class JsonValidator
 {
     public static function isValidJson(string $json): bool
@@ -13,5 +15,33 @@ class JsonValidator
         }
 
         return \json_decode($json, true) && json_last_error() === JSON_ERROR_NONE;
+    }
+
+    public static function decodeValidationError(): bool|JsonDecodeException
+    {
+        return match (json_last_error()) {
+            JSON_ERROR_NONE => false,
+            JSON_ERROR_DEPTH => new JsonDecodeException(
+                'The maximum stack depth was exceeded.',
+            ),
+            JSON_ERROR_STATE_MISMATCH => new JsonDecodeException(
+                'Malformed Json.'
+            ),
+            JSON_ERROR_CTRL_CHAR => new JsonDecodeException(
+                'Unexpected control character was found within the Json string.'
+            ),
+            JSON_ERROR_SYNTAX => new JsonDecodeException(
+                'Syntax error.'
+            ),
+            JSON_ERROR_UTF8 => new JsonDecodeException(
+                'Invalid UTF-8 characters.'
+            ),
+            default => new JsonDecodeException(
+                 sprintf(
+                    'The following decoding error was encountered: `%s`',
+                    json_last_error_msg()
+                 )
+            ),
+        };
     }
 }
