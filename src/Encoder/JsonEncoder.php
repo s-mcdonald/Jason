17728c9dc4e07dc33e858a1ddb9919b934414292
sam <s.mcdonald@outlook.com.au>
2023-05-29 20:47:16 +1000

    update: add encoder

---
+++ src/Encoder/JsonEncoder.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SamMcDonald\Jason\Encoder;
+
+use SamMcDonald\Jason\Contracts\EncoderInterface;
+use SamMcDonald\Jason\Contracts\ResultInterface;
+use SamMcDonald\Jason\Encoder\Result\InValidEncodeResult;
+use SamMcDonald\Jason\Encoder\Result\ValidEncodeResult;
+use SamMcDonald\Jason\Validator\JsonValidator;
+
+class JsonEncoder implements EncoderInterface
+{
+    public function __construct(
+        private int $depth = 512,
+        private int $flags = 0
+    ) {
+    }
+
+    public function encode($value): ResultInterface
+    {
+        $decoded = json_encode($value, $this->depth, $this->flags);
+        if (JsonValidator::hasEncodeValidationError()) {
+            return new InValidEncodeResult(json_last_error_msg());
+        }
+
+        return new ValidEncodeResult($decoded);
+    }
+}
