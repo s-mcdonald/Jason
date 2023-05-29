80ec95bb48afe8a332a761d9e86847df8d7bd970
sam <s.mcdonald@outlook.com.au>
2023-05-29 20:45:32 +1000

    update: Add encoding interfaces

---
+++ src/Encoder/Result/AbstractEncodeResult.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SamMcDonald\Jason\Encoder\Result;
+
+use SamMcDonald\Jason\Contracts\ResultInterface;
+
+abstract class AbstractEncodeResult implements ResultInterface
+{
+    public function __construct(
+        protected mixed $body,
+        protected string $message = '',
+        protected bool $isValid = false
+    ) {
+    }
+
+    final public function getBody(): mixed
+    {
+        return $this->body;
+    }
+
+    final public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    final public function isValid(): bool
+    {
+        return $this->isValid;
+    }
+}
